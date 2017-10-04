3d3132f49de3d515233ed170869a24b9968d9a24
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-04 15:04:29 +0800

    update maze testdata tester

--- hw-2-1003-maze-testdata.cpp
+++ hw-2-1003-maze-testdata.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#define MAX_R 20
+#define MAX_C 20 
 using namespace std;
 int RAND(int a, int b) {
 	return rand()%(b-a+1)+a;
@@ -8,8 +10,8 @@ int RAND(int a, int b) {
 int main(int argc, char* argv[]) {
 	srand(time(NULL));
 	int r, c, sx, sy, ex, ey;
-	r = RAND(2, 10);
-	c = RAND(2, 10);
+	r = RAND(2, MAX_R);
+	c = RAND(2, MAX_C);
 	sx = RAND(1, r);
 	sy = RAND(1, c);
 	ex = RAND(1, r);
--- hw-2-1003-maze-tester.cpp
+++ hw-2-1003-maze-tester.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 int main(int argc, char* argv[]) {
 	string s = argv[1];
-	s += " < hw-2-1003-maze.in";
+	s += " < hw-2-1003-maze.in >  hw-2-1003-maze.out";
 	while(true) {
 		system("hw-2-1003-maze-testdata > hw-2-1003-maze.in");
 		system("type hw-2-1003-maze.in");
 		cout<<endl;
 		system(s.c_str());
+		system("type hw-2-1003-maze.out");
 		system("pause");
 		system("cls");
 	}
