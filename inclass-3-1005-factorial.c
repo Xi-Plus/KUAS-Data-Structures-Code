2a3716f65721e6af041b6c913355a1465932d001
Xi-Plus <huangxuanyuxiplus@gmail.com>
2017-10-05 10:21:11 +0800

    add factorial

---
+++ inclass-3-1005-factorial.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+long long f(long long n) {
+	if(n) return f(n-1)*n;
+	else return 1;
+}
+int main() {
+	int n;
+	while(~scanf("%d", &n)) {
+		printf("%lld\n", f(n));
+	}
+}
+
