7bc6662952ef1cb9ae87f1ba2b60008bc4d40a4b
Xi-Plus <huangxuanyuxiplus@gmail.com>
2017-10-05 11:10:08 +0800

    factorial add sample output

--- inclass-3-1005-factorial.c
+++ inclass-3-1005-factorial.c
@@ -4,6 +4,9 @@ long long f(long long n) {
 	else return 1;
 }
 int main() {
+	for(int q=0; q<=20; q++) {
+		printf("%d %lld\n", q, f(q));
+	}
 	int n;
 	while(~scanf("%d", &n)) {
 		printf("%lld\n", f(n));
